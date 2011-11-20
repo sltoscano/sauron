43d62702740977ccea33499b084a308e48f75405
fatjawa <fatjawa@madlib>
2011-11-20 14:05:50 -0800

    Fixing spacing.

--- sauron-server/server.cpp
+++ sauron-server/server.cpp
@@ -14,85 +14,85 @@
 
 int main()
 {
-	printf("starting server...\n");
-	
-	int sock;
-	int connected;
-	int bytes_recieved;
-	int result = 1;
+  printf("starting server...\n");
+  
+  int sock;
+  int connected;
+  int bytes_recieved;
+  int result = 1;
 
-	char recv_data[PACKETSIZE];
+  char recv_data[PACKETSIZE];
 
-	sockaddr_in server_addr;
-	sockaddr_in client_addr;
-	socklen_t sin_size;
+  sockaddr_in server_addr;
+  sockaddr_in client_addr;
+  socklen_t sin_size;
 
-	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-	{
-		perror("Socket");
-		exit(1);
-	}
+  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+  {
+    perror("Socket");
+    exit(1);
+  }
 
-	if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &result, sizeof(int)) == -1)
-	{
-		perror("Setsockopt");
-		exit(1);
-	}
+  if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &result, sizeof(int)) == -1)
+  {
+    perror("Setsockopt");
+    exit(1);
+  }
 
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(6969);
-	server_addr.sin_addr.s_addr = INADDR_ANY;
-	bzero(&server_addr.sin_zero, 8);
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(6969);
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  bzero(&server_addr.sin_zero, 8);
 
-	if (bind(sock, (sockaddr *)&server_addr, sizeof(sockaddr)) == -1)
-	{
-		perror("unable to bind");
-		exit(1);
-	}
+  if (bind(sock, (sockaddr *)&server_addr, sizeof(sockaddr)) == -1)
+  {
+    perror("unable to bind");
+    exit(1);
+  }
 
-	if (listen(sock, 5) == -1)
-	{
-		perror("Listen");
-		exit(1);
-	}
+  if (listen(sock, 5) == -1)
+  {
+    perror("Listen");
+    exit(1);
+  }
 
-	printf("\nTCPServer waiting for client on port 6969");
-	fflush(stdout);
+  printf("\nTCPServer waiting for client on port 6969");
+  fflush(stdout);
 
-	while(1)
-	{
-		sin_size = sizeof(sockaddr_in);
+  while(1)
+  {
+    sin_size = sizeof(sockaddr_in);
 
-		connected = accept(sock, (struct sockaddr *)&client_addr, &sin_size);
-		
-		printf("\nGot a connection from: (%s, %d)",
-			inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+    connected = accept(sock, (struct sockaddr *)&client_addr, &sin_size);
+    
+    printf("\nGot a connection from: (%s, %d)",
+      inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
-		fflush(stdout);
+    fflush(stdout);
 
-		while (1)
-		{
-			char* recv_pos = (char*) &recv_data;
+    while (1)
+    {
+      char* recv_pos = (char*) &recv_data;
 
-			bytes_recieved = recv(connected, recv_data, PACKETSIZE, 0);
+      bytes_recieved = recv(connected, recv_data, PACKETSIZE, 0);
 
-			while (bytes_recieved < PACKETSIZE)
-			{
-				recv_pos += bytes_recieved;
-				bytes_recieved += recv(connected, recv_pos, PACKETSIZE, 0);
-			}
+      while (bytes_recieved < PACKETSIZE)
+      {
+        recv_pos += bytes_recieved;
+        bytes_recieved += recv(connected, recv_pos, PACKETSIZE, 0);
+      }
 
-			for (int i=0; i<PACKETSIZE; ++i)
-			{
-				printf("%x ", recv_data[i]);
-			}
-			printf("\n");
+      for (int i=0; i<PACKETSIZE; ++i)
+      {
+        printf("%x ", recv_data[i]);
+      }
+      printf("\n");
 
-			fflush(stdout);
-		}
-	}
+      fflush(stdout);
+    }
+  }
 
-	close(connected);
-	close(sock);
-	return 0;
+  close(connected);
+  close(sock);
+  return 0;
 }
